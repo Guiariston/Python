fba226a71f866abb13e2a416ef44cc8195bd600f
Guilherme Ariston Portes <gui-ariston@outlook.com>
2026-05-27 00:25:15 +0000

    Atualizando Projeto Python Django

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username = "testuser", email="test@email.com", password="secret"
+        )
+        cls.post = Post.objects.create(
+            title = "A good title",
+            body = "Nice body content",
+            author = cls.user,
+        )
